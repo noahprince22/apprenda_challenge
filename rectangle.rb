ede27cdd736e8d57ce7d610cf61fbe8fb1d9358c
Noah Prince <nprince@Noahs-MacBook-Pro.local>
2015-09-11 19:55:57 -0500

    Add rectangle

---
+++ rectangle.rb
@@ -0,0 +1,24 @@
+require_relative "point.rb"
+
+require 'pry'
+
+class Rectangle
+	attr_accessor :location, :width, :height
+
+	# initializes a rectangle
+  	#
+  	# ==== Parameters
+ 	#
+ 	# * +location+ - the point location of the bottom left corner
+ 	# * +width+ - the width of the rectangle
+ 	# * +height+ - the height of the rectangle
+	def initialize(location, width, height)
+		throw "NillLocationError" unless location
+		throw "WidthError" unless width > 0
+		throw "HeightError" unless height > 0
+
+		self.location = location
+		self.width = width
+		self.height = height
+	end
+end
---
+++ spec/rectangle.rb
@@ -0,0 +1,10 @@
+require_relative "point.rb"
+
+class Rectangle
+	attr_accessor :points
+
+	def init(pts)
+		self.points = pts
+	end
+
+end
