d9813b71dc9756faa95dbb4c08ee2ac1bd98bb1d
Noah Prince <nprince@Noahs-MacBook-Pro.local>
2015-09-12 14:05:25 -0500

    Fix point comment

--- point.rb
+++ point.rb
@@ -1,5 +1,3 @@
-# Currently only supports integers. DO NOT use doubles or floats, 
-#    or there will be undefined behavior
 Point = Struct.new(:x, :y) {
 	def ==(other) 
 		return (other.x == self.x and other.y == self.y)
